05eeafc683f990b436455c2aac17895d2acfef56
Zafer AYAN <ozcanzaferayan@gmail.com>
2024-03-30 15:23:36 +0300

    Fix cucumber

---
+++ features/support/steps.ts
@@ -0,0 +1,11 @@
+import { Then, When } from "@cucumber/cucumber";
+import assert from "assert";
+import { sayHello } from "./greeting.ts";
+
+When("the greeter says hello", function () {
+  this.whatIHeard = sayHello();
+});
+
+Then("I should have heard {string}", function (expectedResponse) {
+  assert.equal(this.whatIHeard, expectedResponse);
+});
